425989e3adf5e73023bcb691881da39736c4c557
Leonardo Lima de Vasconcellos <leo.lima.web@gmail.com>
2022-06-15 20:59:22 -0300

    Final implementation

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,10 @@
+import 'dart:async';
+import 'dart:collection';
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
 const List<String> kCurrenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +35,42 @@ const List<String> kCryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  final String _url = 'https://rest.coinapi.io/v1/exchangerate';
+  final Map<String, String> _header = {
+    'X-CoinAPI-Key': 'AF2B054A-3861-415C-9005-C372E8D57B91'
+  };
+
+  final HashMap<String, HashMap<String, double>> _rates = HashMap();
+
+  Future<double> getRate(String crypto, String currency) async {
+    if ((!_rates.containsKey(crypto)) || (!_rates[crypto]!.containsKey(currency))) {
+      await _getData(crypto, currency);
+    }
+    return _rates[crypto]![currency]!;
+  }
+
+  Future<void> _getData(String crypto, String currency) async {
+    final response = await http.get(
+      Uri.parse('$_url/$crypto/$currency'),
+      headers: _header,
+    );
+    if (response.statusCode != 200) {
+      if (kDebugMode) {
+        print(response.statusCode);
+      }
+      return;
+    }
+    var data = jsonDecode(response.body);
+    double rate = data['rate'];
+    HashMap<String, double> quote = HashMap();
+    quote.addAll({currency : rate});
+    if (!_rates.containsKey(crypto)) {
+      _rates.addAll({data['asset_id_base']: quote});
+    } else {
+      if (!_rates[crypto]!.containsKey(currency)) {
+        _rates[crypto]?.addAll(quote);
+      }
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -13,6 +13,10 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String _selectedCurrency = 'USD';
+  late CoinData _coinData;
+  double _BTCRate = 0;
+  double _ETHRate = 0;
+  double _LTCRate = 0;
 
   Widget getPicker() {
     if (Platform.isIOS) {
@@ -20,9 +24,7 @@ class _PriceScreenState extends State<PriceScreen> {
         backgroundColor: Colors.lightBlue,
         itemExtent: 32,
         onSelectedItemChanged: (index) {
-          setState(() {
-            _selectedCurrency = kCurrenciesList[index];
-          });
+          _getData(kCurrenciesList[index]);
         },
         children: <Widget>[
           for (var item in kCurrenciesList) Text(item),
@@ -32,9 +34,7 @@ class _PriceScreenState extends State<PriceScreen> {
       return DropdownButton<String>(
         value: _selectedCurrency,
         onChanged: (value) {
-          setState(() {
-            _selectedCurrency = value!;
-          });
+          _getData(value!);
         },
         items: <DropdownMenuItem<String>>[
           for (var item in kCurrenciesList)
@@ -48,6 +48,25 @@ class _PriceScreenState extends State<PriceScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _coinData = CoinData();
+    _getData(_selectedCurrency);
+  }
+
+  void _getData(String currency) async {
+    double btcRate = await _coinData.getRate('BTC', currency);
+    double ethRate = await _coinData.getRate('ETH', currency);
+    double ltcRate = await _coinData.getRate('LTC', currency);
+    setState(() {
+      _selectedCurrency = currency;
+      _BTCRate = btcRate;
+      _ETHRate = ethRate;
+      _LTCRate = ltcRate;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -57,26 +76,20 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          ExchangeRate(
+            crypto: 'BTC',
+            currency: _selectedCurrency,
+            rate: _BTCRate,
+          ),
+          ExchangeRate(
+            crypto: 'ETH',
+            currency: _selectedCurrency,
+            rate: _ETHRate,
+          ),
+          ExchangeRate(
+            crypto: 'LTC',
+            currency: _selectedCurrency,
+            rate: _LTCRate,
           ),
           Container(
             height: 150.0,
@@ -86,6 +99,44 @@ class _PriceScreenState extends State<PriceScreen> {
             child: getPicker(),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ExchangeRate extends StatelessWidget {
+  final String currency;
+  final String crypto;
+  final double rate;
+
+  const ExchangeRate(
+      {Key? key,
+      required this.currency,
+      required this.crypto,
+      required this.rate})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $crypto = ${rate.toStringAsFixed(2)} $currency',
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
