1b839a2bb9d2d7507235759e10c341d6bfd21eb3
Leonardo Lima de Vasconcellos <leo.lima.web@gmail.com>
2022-06-15 17:53:46 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:bitcoin_ticker/price_screen.dart';
+
+void main() {
+  runApp(const BitcoinTickerApp());
+}
+
+class BitcoinTickerApp extends StatelessWidget {
+  const BitcoinTickerApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.lightBlue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const PriceScreen(),
+    );
+  }
+}
